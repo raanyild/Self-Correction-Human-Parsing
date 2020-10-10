1b53a23f9f6246e0467d943029d8239a7bb3ef5b
raanyild <raanyild@gmail.com>
2020-10-10 13:16:33 +0530

    output formating color code change

--- simple_extractor.py
+++ simple_extractor.py
@@ -74,17 +74,9 @@ def get_palette(num_cls):
     n = num_cls
     palette = [0] * (n * 3)
     for j in range(0, n):
-        lab = j
-        palette[j * 3 + 0] = 0
-        palette[j * 3 + 1] = 0
-        palette[j * 3 + 2] = 0
-        i = 0
-        while lab:
-            palette[j * 3 + 0] |= (((lab >> 0) & 1) << (7 - i))
-            palette[j * 3 + 1] |= (((lab >> 1) & 1) << (7 - i))
-            palette[j * 3 + 2] |= (((lab >> 2) & 1) << (7 - i))
-            i += 1
-            lab >>= 3
+        palette[j * 3 + 0] = j
+        palette[j * 3 + 1] = j
+        palette[j * 3 + 2] = j
     return palette
 
 
